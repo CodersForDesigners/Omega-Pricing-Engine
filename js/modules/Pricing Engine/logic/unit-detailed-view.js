e8d5e8dcae916c17f0a312e2514777cab5f2c2a8
adi <adityabhat@lazaro.in>
2018-11-08 13:36:49 +0530

    Unit Detailed View : Hold the render for the modification checkbox animation

--- js/modules/Pricing Engine/logic/unit-detailed-view.js
+++ js/modules/Pricing Engine/logic/unit-detailed-view.js
@@ -13,72 +13,84 @@
  */
 $( document ).on( "unit/view", function ( event, data ) {
 
-	// 1. Render stub UIs for the Unit Detailed View and EMI Calculator
-	$( document ).trigger( "unit-details/render" );
-	$( document ).trigger( "emi-calculator/render" );
-
-	// 2. Save the user input data on the side.
 	var newUnitData = data.unitData;
-	__OMEGA.userInput.unitData = Object.assign( __OMEGA.userInput.unitData, newUnitData );
 
-	// 3. Run the computations through the pricing engine
-	computeUnitData();
-	var points = getComputedUnitData();
+	// Get the context under which this event was triggered
+	// If it was a modification ( specifically, a binary one ),
+		// then wait for teeny bit ( 'cause animation )
+	var waitTimeForAnimation = 0;
+	if ( newUnitData && newUnitData.Unit == void 0 )
+		waitTimeForAnimation = 0.31;	// 350 milli-seconds
 
-	// Filter out the modifications that aren't applicable for the unit
-	// 	and set the unit-specific values for them ( if present )
-	var modifications = __OMEGA.modifications;
-	var selectedUnitNumber = __OMEGA.userInput.unitData.Unit;
-	var selectedUnit = __OMEGA.units.find( function ( unit ) {
-		return unit.Unit == selectedUnitNumber;
-	} );
-	points = points
-		.filter( function ( point ) {
-			// This part may be a bit tricky to follow.
-			// Firstly, if the line item **does not** represent a modification, let it through
-			if ( ! point.Modifiable ) return true;
-			// If it **does** represent a modification, and if it is applicable to for this unit, let it through
-			if ( selectedUnit.modifications[ point.Name ] ) return true;
-			// Else, do not let it through
-			return false;
-		} )
-		.map( function ( point ) {
-			if ( ! point.Modifiable )
-				return point;
-			point.modification = selectedUnit.modifications[ point.Name ];
-			return point;
+	waitFor( waitTimeForAnimation ).then( function () {
+
+		// 1. Render stub UIs for the Unit Detailed View and EMI Calculator
+		$( document ).trigger( "unit-details/render" );
+		$( document ).trigger( "emi-calculator/render" );
+
+		// 2. Save the user input data on the side.
+		__OMEGA.userInput.unitData = Object.assign( __OMEGA.userInput.unitData, newUnitData );
+
+		// 3. Run the computations through the pricing engine
+		computeUnitData();
+		var points = getComputedUnitData();
+
+		// Filter out the modifications that aren't applicable for the unit
+		// 	and set the unit-specific values for them ( if present )
+		var modifications = __OMEGA.modifications;
+		var selectedUnitNumber = __OMEGA.userInput.unitData.Unit;
+		var selectedUnit = __OMEGA.units.find( function ( unit ) {
+			return unit.Unit == selectedUnitNumber;
 		} );
+		points = points
+			.filter( function ( point ) {
+				// This part may be a bit tricky to follow.
+				// Firstly, if the line item **does not** represent a modification, let it through
+				if ( ! point.Modifiable ) return true;
+				// If it **does** represent a modification, and if it is applicable to for this unit, let it through
+				if ( selectedUnit.modifications[ point.Name ] ) return true;
+				// Else, do not let it through
+				return false;
+			} )
+			.map( function ( point ) {
+				if ( ! point.Modifiable )
+					return point;
+				point.modification = selectedUnit.modifications[ point.Name ];
+				return point;
+			} );
 
-	// 4. Store the grand total value on the global state
-	var grandTotal;
-	points.some( function ( point ) {
-		if ( point.Content == "Grand Total" ) {
-			grandTotal = parseFloat( point.Value );
-			return true;
-		}
+		// 4. Store the grand total value on the global state
+		var grandTotal;
+		points.some( function ( point ) {
+			if ( point.Content == "Grand Total" ) {
+				grandTotal = parseFloat( point.Value );
+				return true;
+			}
+		} );
+		__OMEGA.unitData.grandTotal = grandTotal;
+
+		// 5. Render the Unit Details UI
+		var eventData = {
+			unitNumber: selectedUnitNumber,
+			points: points
+		};
+			// Get the context
+		if ( data.context )
+			eventData.context = data.context;
+		// If something other than the unit number changed,
+			// it must be a modification
+		else if ( newUnitData && newUnitData.Unit == void 0 )
+			eventData.context = "modification";
+
+		$( document ).trigger( "unit-details/render", eventData );
+
+		// 6. Render the EMI Calculator UI
+		$( document ).trigger( "emi-calculator/render", { total: grandTotal } );
+
+		// 7. Expose a hook
+		$( document ).trigger( "unit/view/done", { total: grandTotal } );
+
 	} );
-	__OMEGA.unitData.grandTotal = grandTotal;
-
-	// 5. Render the Unit Details UI
-	var eventData = {
-		unitNumber: selectedUnitNumber,
-		points: points
-	};
-		// Get the context
-	if ( data.context )
-		eventData.context = data.context;
-	// If something other than the unit number changed,
-		// it must be a modification
-	else if ( newUnitData && newUnitData.Unit == void 0 )
-		eventData.context = "modification";
-
-	$( document ).trigger( "unit-details/render", eventData );
-
-	// 6. Render the EMI Calculator UI
-	$( document ).trigger( "emi-calculator/render", { total: grandTotal } );
-
-	// 7. Expose a hook
-	$( document ).trigger( "unit/view/done", { total: grandTotal } );
 
 } );
 
