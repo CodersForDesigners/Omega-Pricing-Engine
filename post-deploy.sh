aab39fd3c4910f45c875806c0de1338bc117a447
adi <adityabhat@lazaro.in>
2019-01-17 17:12:44 +0530

    Spreadsheet : Automatically import the sheet from Google Sheets at 2am daily

--- post-deploy.sh
+++ post-deploy.sh
@@ -9,10 +9,33 @@ while getopts "p:" opt; do
 	esac
 done
 
-# Establish symbolic links to the `media`, `account` and `favicon` directories
+# Establish symbolic links for the following directories:
+# the media folder
 rm media
 ln -s ../media/${PROJECT_DIR} media
+# the configuration folder
+rm configuration
+ln -s ../configuration/${PROJECT_DIR} configuration
+# the account folder
 rm account
 ln -s ../accounts/${PROJECT_DIR} account
+# the favicon folder
 rm favicon
 ln -s ../media/${PROJECT_DIR}/favicon favicon
+# the logs folder
+rm logs
+ln -s ../data/${PROJECT_DIR}/logs logs
+
+
+# Set up all the scheduled tasks with cron
+chmod 744 setup/import-pricing-sheet.php
+php setup/import-pricing-sheet.php
+
+CURRENT_WORKING_DIR=`pwd`
+HOME=${CURRENT_WORKING_DIR}/logs
+CRON_ENV="\n\nPATH=/bin:/usr/bin:/usr/local/bin:${CURRENT_WORKING_DIR}/setup\nHOME=${HOME}\n";
+printf $CRON_ENV | cat - setup/tasks.crontab | tee tmp__crontab;
+rm setup/tasks.crontab;
+mv tmp__crontab setup/tasks.crontab;
+cp setup/tasks.crontab ../cronjobs/$PROJECT_DIR.crontab
+cat ../cronjobs/*.crontab | crontab -
---
+++ setup/import-pricing-sheet.php
@@ -0,0 +1,106 @@
+#!/usr/bin/php
+<?php
+
+ini_set( "display_errors", 'stderr' );
+ini_set( "error_reporting", E_ALL );
+
+// Set the timezone
+date_default_timezone_set( 'Asia/Kolkata' );
+// Do not let this script timeout
+set_time_limit( 0 );
+
+
+
+
+
+
+
+
+
+// Allow this to run in the command line only
+// if ( php_sapi_name() != 'cli' )
+	// throw new Exception( 'This application must be run on the command line.' );
+
+require_once __DIR__ . '/../vendor/autoload.php';
+
+
+function getClient () {
+
+	$client = new Google_Client();
+	$client->setApplicationName( 'Omega Pricing Sheet Import' );
+	$client->setScopes( Google_Service_Drive::DRIVE );
+	$authCredentialsFilename = __DIR__ . '/../configuration/google-credentials.json';
+	$client->setAuthConfig( $authCredentialsFilename );
+	$client->setAccessType( 'offline' );
+	$client->setPrompt( 'select_account consent' );
+
+	/*
+	 * Load previously authorized token from a file, if it exists.
+	 * The file token.json stores the user's access and refresh tokens, and is
+	 * created automatically when the authorization flow completes for the first
+	 * time.
+	 */
+	$tokenPath = __DIR__ . '/../configuration/google-token.json';
+	if ( file_exists( $tokenPath ) ) {
+		$accessToken = json_decode( file_get_contents( $tokenPath ), true );
+		$client->setAccessToken( $accessToken );
+	}
+
+	// If an access token exists and is not expired, return
+	if ( ! $client->isAccessTokenExpired() )
+		return $client;
+
+	// If an access token does not exist or is expired,
+		// Refresh the token if possible
+	if ( $client->getRefreshToken() ) {
+		$client->fetchAccessTokenWithRefreshToken( $client->getRefreshToken() );
+		return $client;
+	}
+
+		// Else, fetch a new one
+
+	// Request authorization from the user.
+	$authUrl = $client->createAuthUrl();
+	printf( 'Open the following link in your browser:\n%s\n', $authUrl );
+	print 'Enter verification code: ';
+	$authCode = trim( fgets( STDIN ) );
+	// Exchange authorization code for an access token.
+	$accessToken = $client->fetchAccessTokenWithAuthCode( $authCode );
+	$client->setAccessToken( $accessToken );
+	// Check to see if there was an error.
+	if ( array_key_exists( 'error', $accessToken ) )
+		throw new Exception( join( ', ', $accessToken ) );
+	// Save the token to a file.
+	if ( ! file_exists( dirname( $tokenPath ) ) )
+		mkdir( dirname( $tokenPath ), 0700, true );
+	file_put_contents( $tokenPath, json_encode( $client->getAccessToken(), JSON_PRETTY_PRINT ) );
+
+	return $client;
+
+}
+
+function main () {
+
+	$client = getClient();
+	$service = new Google_Service_Drive( $client );
+
+	$pricingSheetMeta = json_decode( file_get_contents( __DIR__ . '/../configuration/pricing-sheet.json' ), true );
+	$fileId = $pricingSheetMeta[ 'id' ];
+	$response = $service->files->export(
+		$fileId,
+		'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet',
+		[ 'alt' => 'media' ]
+	);
+	$content = $response->getBody()->getContents();
+
+	// Write the contents to disk
+	$outputFilename = __DIR__ . '/../account/data/numbers.xlsx';
+	file_put_contents( $outputFilename, $content );
+
+}
+
+
+
+
+
+main();
