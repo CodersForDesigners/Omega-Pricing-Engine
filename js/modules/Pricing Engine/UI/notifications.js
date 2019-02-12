8053ebaf3e069db7834dda45c013d7aefd8f515f
adi <adityabhat@lazaro.in>
2019-02-12 11:49:23 +0530

    Unit Details : Conditionally show the Sticky Header and Footer for laptops
    The header is the "To see all available Apartments, Click Here".
    The footer is the Grand Total.

--- js/modules/Pricing Engine/UI/notifications.js
+++ js/modules/Pricing Engine/UI/notifications.js
@@ -27,3 +27,41 @@ $( document ).on( "click", ".js_notification_section .js_notification_close", fu
 	$notification.remove();
 
 } );
+
+/*
+ *
+ * On devices of a certain height,
+ * 	hide the "To see all available Apartments, Click Here" on scrolling down
+ *
+ */
+$( function () {
+
+	if ( $( "html" ).hasClass( "page-pricing-individual" ) ) {
+
+		var toggleNavigationBar = function () {
+
+			var lastScrollTopPosition = window.scrollY || document.body.scrollTop
+			var $navigationBar = __UI.$notificationSection.find( ".js_user_bar" );
+
+			return function toggleNavigationBar () {
+
+				var currentScrollTopPosition = window.scrollY || document.body.scrollTop;
+
+				// When scrolling down, hide the navigation bar
+				if ( currentScrollTopPosition > lastScrollTopPosition )
+					$navigationBar.addClass( "hide" );
+				// When scrolling up, show the navigation bar
+				else if ( currentScrollTopPosition < lastScrollTopPosition )
+					$navigationBar.removeClass( "hide" );
+
+				lastScrollTopPosition = currentScrollTopPosition;
+
+			};
+
+		}();
+
+		$( window ).on( "scroll", toggleNavigationBar );
+
+	}
+
+} );
