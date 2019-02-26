f868feef0bd1c41c71e1e1a870e30cbbec7db105
adi <adityabhat@lazaro.in>
2019-02-26 16:03:42 +0530

    Tracking : Log a note when users visit the index Pricing page
    Don't keep logging simply when they visit the page.
    If there's a gap of more than an hour between successive visits, then log a note.

--- js/modules/tracking.js
+++ js/modules/tracking.js
@@ -7,8 +7,37 @@
  */
 $( document ).on( "user/logged-in", function ( event, data ) {
 
-	var noteContent = "Customer viewed the Pricing Engine";
-	addNoteToUser( "Omega Customer Insights", noteContent );
+	var noteContent = "Omega Event Log";
+	addNoteToUser( "Customer SEARCHED for Units on the Listing Page.", noteContent );
+
+} );
+
+/*
+ *
+ * On viewing ( specifically, after rendering ) the "index" Pricing page,
+ *	if the user is already logged in and
+ * 		if the user was last seen **more than an hour ago**
+ * 	then log a note to the user's record on the CRM
+ *
+ */
+$( document ).on( "pricing-engine/render/after", function ( event, data ) {
+
+	if ( $( "html" ).data( "page" ) != "page-pricing-index" )
+		return;
+
+	getUser().then( function ( user ) {
+
+		if ( user.lastSeenAt )
+			if ( Date.now() - parseInt( user.lastSeenAt, 10 ) < ( 60 * 60 * 1000 ) )
+				return;
+
+		var noteContent = "Customer SEARCHED for Units on the Listing Page.";
+		addNoteToUser( "Omega Event Log", noteContent );
+		// Update the user object "cookie" entry with a fresh `lastSeenAt` timestamp
+		user.lastSeenAt = Date.now();
+		loginUser( user );
+
+	} );
 
 } );
 
